c1c880a5c4724b7f448e390c2a2e62030316816f
masqu <masqu@DESKTOP-N01K9GN.lan>
2025-11-10 18:19:32 +0000

    base

---
+++ FishFilletsPOO_base_numero_nome_numero2_nome2/src/objects/GameCharacter.java
@@ -0,0 +1,26 @@
+package objects;
+
+import java.util.Random;
+
+import pt.iscte.poo.game.Room;
+import pt.iscte.poo.utils.Point2D;
+import pt.iscte.poo.utils.Vector2D;
+
+public abstract class GameCharacter extends GameObject {
+	
+	public GameCharacter(Room room) {
+		super(room);
+	}
+	
+	public void move(Vector2D dir) {
+		Random rand = new Random();
+		Point2D destination = new Point2D(rand.nextInt(10), rand.nextInt(10)); 
+		setPosition(destination);		
+	}
+
+	@Override
+	public int getLayer() {
+		return 2;
+	}
+	
+}
---
+++ FishFilletsPOO_base_numero_nome_numero2_nome2/src/pt/iscte/poo/game/GameEngine.java
@@ -0,0 +1,62 @@
+package pt.iscte.poo.game;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import objects.SmallFish;
+import objects.BigFish;
+import pt.iscte.poo.gui.ImageGUI;
+import pt.iscte.poo.observer.Observed;
+import pt.iscte.poo.observer.Observer;
+import pt.iscte.poo.utils.Direction;
+
+public class GameEngine implements Observer {
+	
+	private Map<String,Room> rooms;
+	private Room currentRoom;
+	private int lastTickProcessed = 0;
+	
+	public GameEngine() {
+		rooms = new HashMap<String,Room>();
+		loadGame();
+		currentRoom = rooms.get("room0.txt");
+		updateGUI();		
+		SmallFish.getInstance().setRoom(currentRoom);
+		BigFish.getInstance().setRoom(currentRoom);
+	}
+
+	private void loadGame() {
+		File[] files = new File("./rooms").listFiles();
+		for(File f : files) {
+			rooms.put(f.getName(),Room.readRoom(f,this));
+		}
+	}
+
+	@Override
+	public void update(Observed source) {
+
+		if (ImageGUI.getInstance().wasKeyPressed()) {
+			int k = ImageGUI.getInstance().keyPressed();
+			SmallFish.getInstance().move(Direction.directionFor(k).asVector());
+			BigFish.getInstance().move(Direction.directionFor(k).asVector());
+		}
+		int t = ImageGUI.getInstance().getTicks();
+		while (lastTickProcessed < t) {
+			processTick();
+		}
+		ImageGUI.getInstance().update();
+	}
+
+	private void processTick() {		
+		lastTickProcessed++;
+	}
+
+	public void updateGUI() {
+		if(currentRoom!=null) {
+			ImageGUI.getInstance().clearImages();
+			ImageGUI.getInstance().addImages(currentRoom.getObjects());
+		}
+	}
+	
+}
