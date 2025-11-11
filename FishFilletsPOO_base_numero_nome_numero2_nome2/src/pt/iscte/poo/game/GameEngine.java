279083cfecb26b3f9eec63a018cdd56cfbc5fe31
masqu <masqu@LAPTOP-QRNSAG1I>
2025-11-11 10:50:15 +0000

    Implementar a interação de cada peixe com um objeto que pode mover e um
    objeto que não
    pode mover

--- FishFilletsPOO_base_numero_nome_numero2_nome2/src/pt/iscte/poo/game/GameEngine.java
+++ FishFilletsPOO_base_numero_nome_numero2_nome2/src/pt/iscte/poo/game/GameEngine.java
@@ -3,42 +3,52 @@ package pt.iscte.poo.game;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import objects.SmallFish;
+import objects.SteelHorizontal;
+import objects.SteelVertical;
+import objects.Trunk;
+import objects.Wall;
 import objects.BigFish;
+import objects.GameCharacter;
+import objects.GameObject;
+import objects.HoledWall;
 import pt.iscte.poo.gui.ImageGUI;
 import pt.iscte.poo.observer.Observed;
 import pt.iscte.poo.observer.Observer;
 import pt.iscte.poo.utils.Direction;
+import pt.iscte.poo.utils.Point2D;
 
 public class GameEngine implements Observer {
-	
-	private Map<String,Room> rooms;
+
+	private Map<String, Room> rooms;
 	private Room currentRoom;
 	private int lastTickProcessed = 0;
 	private boolean isSmallFishTurn;
-	
+
 	public GameEngine() {
-		rooms = new HashMap<String,Room>();
+		rooms = new HashMap<String, Room>();
 		loadGame();
 		currentRoom = rooms.get("room0.txt");
-		updateGUI();		
+		updateGUI();
 		SmallFish.getInstance().setRoom(currentRoom);
 		BigFish.getInstance().setRoom(currentRoom);
-		this.isSmallFishTurn=true;
+		this.isSmallFishTurn = true;
 	}
+
 	public String isSmallFishTurn() {
-		if(isSmallFishTurn)
+		if (isSmallFishTurn)
 			return "SmallFish turn";
-		return "BigFish turn";		
-			
+		return "BigFish turn";
+
 	}
 
 	private void loadGame() {
 		File[] files = new File("./rooms").listFiles();
-		for(File f : files) {
-			rooms.put(f.getName(),Room.readRoom(f,this));
+		for (File f : files) {
+			rooms.put(f.getName(), Room.readRoom(f, this));
 		}
 	}
 
@@ -46,17 +56,28 @@ public class GameEngine implements Observer {
 	public void update(Observed source) {
 
 		if (ImageGUI.getInstance().wasKeyPressed()) {
-	        int k = ImageGUI.getInstance().keyPressed();
-	        if (k == KeyEvent.VK_SPACE) {
-	            isSmallFishTurn = !isSmallFishTurn;
-	        } else {
-	            if (isSmallFishTurn) {
-	                SmallFish.getInstance().move(Direction.directionFor(k).asVector());
-	            } else {
-	                BigFish.getInstance().move(Direction.directionFor(k).asVector());
-	            }
-	        }
-	    }
+			int k = ImageGUI.getInstance().keyPressed();
+			if (k == KeyEvent.VK_SPACE) {
+				isSmallFishTurn = !isSmallFishTurn;
+			} else {
+				Direction dir = Direction.directionFor(k);
+				
+				if (dir != null) {
+					
+					GameCharacter activeFish;
+					if (isSmallFishTurn) {
+						activeFish = SmallFish.getInstance();
+					} else {
+						activeFish = BigFish.getInstance();
+					}
+
+					Point2D targetPos = activeFish.getPosition().plus(dir.asVector());
+					if (isMoveValid(targetPos)) {
+						activeFish.move(dir.asVector());
+					}
+				}
+			}
+		}
 		int t = ImageGUI.getInstance().getTicks();
 		while (lastTickProcessed < t) {
 			processTick();
@@ -65,15 +86,36 @@ public class GameEngine implements Observer {
 		ImageGUI.getInstance().setStatusMessage(isSmallFishTurn());
 	}
 
-	private void processTick() {		
+	private boolean isMoveValid(Point2D targetPos) {
+
+		List<GameObject> allObjects = currentRoom.getObjects();
+
+		for (GameObject obj : allObjects) {
+			if (!obj.getPosition().equals(targetPos)) {
+				continue;
+			}
+			if (obj instanceof Wall || obj instanceof SteelHorizontal || obj instanceof SteelVertical || obj instanceof GameCharacter) {
+				return false;
+			}
+			
+			if(!isSmallFishTurn) {
+				if (obj instanceof HoledWall || obj instanceof Trunk ) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	private void processTick() {
 		lastTickProcessed++;
 	}
 
 	public void updateGUI() {
-		if(currentRoom!=null) {
+		if (currentRoom != null) {
 			ImageGUI.getInstance().clearImages();
 			ImageGUI.getInstance().addImages(currentRoom.getObjects());
 		}
 	}
-	
+
 }
